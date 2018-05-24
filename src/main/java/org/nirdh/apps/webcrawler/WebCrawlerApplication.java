fc9f2dab0bd4f97cadd19f1d971ea713f2c14fad
Nirdh Mehta <coolnirdh@gmail.com>
2018-05-24 10:21:53 +0530

    Added Spring Integration Flow to orchestrate the lifecycle of crawling.

--- src/main/java/org/nirdh/apps/webcrawler/WebCrawlerApplication.java
+++ src/main/java/org/nirdh/apps/webcrawler/WebCrawlerApplication.java
@@ -2,8 +2,10 @@ package org.nirdh.apps.webcrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("crawler-flow.xml")
 public class WebCrawlerApplication {
 
 	public static void main(String[] args) {
